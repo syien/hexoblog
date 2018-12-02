eade98bb426378802a9104131265235e6ec4194e
syien <32385452+syien@users.noreply.github.com>
2018-12-02 23:55:45 +0800

    Site updated: 2018-12-02 23:55:45

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 		h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
 
-//Search part
+//Search form part
 function searchToggle(obj, evt){
 	var container = $(obj).closest('.search-wrapper');
 
@@ -64,8 +64,8 @@ function searchToggle(obj, evt){
 		$('.article-share-box.on').hide();
 
 		box.css({
-			top: offset.top - 92,
-			left: offset.left - 105
+			top: offset.top - 100,
+			left: offset.left - 187
 		}).addClass('on');
     }).on('click', '.article-share-box', function(e){
 		e.stopPropagation();
